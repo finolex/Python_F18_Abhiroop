6254800554534e59a624fa6c139526c0dfa03c46
finolex <hermitdudz@gmail.com>
2018-01-29 12:39:24 -0500

    python lab 1

---
+++ Lab 1 - Binary and Inputs/1.py
@@ -0,0 +1,8 @@
+print("Hey, Sir!")
+
+first_int = int(input("What would you like the first int to be? "))
+second_int = int(input("What would you like the second int to be? "))
+diff = first_int - second_int
+sum_of  = first_int + second_int
+product = first_int * second_int
+print("diff = ", diff, "sum = ", sum_of, "product = ", product)
